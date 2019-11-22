41963785f0970beefd6cbd2aa2d9d3b75afa3ec4
Randerson Lemos <randerson.lemos@gmail.com>
2019-11-22 10:11:06 -0300

    Adding more path variables for flexibility.

--- scripts/settings.py
+++ scripts/settings.py
@@ -13,18 +13,31 @@ LOCAL_ROOT     = Path('U:/simulation')
 LOCAL_IMEX_EXE = Path('C:/\"Program Files (x86)"/CMG/IMEX/2017.10/Win_x64/EXE/mx201710.exe')
 LOCAL_REPO_EXE = Path('C:/\"Program Files (x86)"/CMG/BR/2017.10/Win_x64/EXE/report.exe')
 LOCAL_PUTT_EXE = Path('C:/\"Program Files (x86)"/PuTTY/plink.exe')
+
+REMOTE_ROOT     = PurePosixPath('/home/randerson/simulation')
+REMOTE_IMEX_EXE = PurePosixPath('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
+
 SIMS_FOLDER = Path('sims')
+
+DAT_ROOT = Pah('U:/simulation')
 DAT_FOLDER = Path('dat')
-RWD_FOLDER = Path('rwd')
 DAT_NAME = Path('main.dat')
-IRF_NAME = Path('main.irf')
-REP_NAME = Path('main.rep')
+
+RWD_ROOT = Pah('U:/simulation')
+RWD_FOLDER = Path('rwd')
 RWD_NAME = Path('main.rwd')
+
+IRF_ROOT = Pah('U:/simulation')
+IRF_FOLDER = Path('rwd')
+IRF_NAME = Path('main.irf')
+
+REP_ROOT = Pah('U:/simulation')
+REP_NAME = Path('main.rep')
+
+INF_ROOT = Pah('U:/simulation')
 INF_NAME = Path('main.inf')
 
 USER = 'randerson'
 CLUSTER_NAME = 'hpc02'
 QUEUE_KIND = 'longas'
 
-REMOTE_ROOT     = PurePosixPath('/home/randerson/simulation')
-REMOTE_IMEX_EXE = PurePosixPath('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
