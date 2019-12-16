b81ff0059fbae117b7c4ded7db81c824666128fc
Randerson Lemos <randerson.lemos@gmail.com>
2019-12-16 17:42:07 -0300

    Fixed path of the simulators.

--- scripts/settings.py
+++ scripts/settings.py
@@ -15,7 +15,7 @@ LOCAL_REPO_EXE = Path('C:/\"Program Files (x86)"/CMG/BR/2017.10/Win_x64/EXE/repo
 LOCAL_PUTT_EXE = Path('C:/\"Program Files (x86)"/PuTTY/plink.exe')
 
 REMOTE_ROOT     = PurePosixPath('/home/randerson/simulation')
-REMOTE_IMEX_EXE = PurePosixPath('/mnt/simuladores/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
+REMOTE_IMEX_EXE = PurePosixPath('/mnt/software/CMG/imex/2017.10/linux_x64/exe/mx201710.exe')
 
 SIMS_FOLDER = Path('sims')
 
